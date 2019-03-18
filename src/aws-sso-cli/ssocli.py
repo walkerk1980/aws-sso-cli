baff3a32e7f9bbe0739643d42743993ecd056ea3
Keith Walker <keiwlk@amazon.com>
2019-03-18 13:29:19 -0700

    modularize main - extract functions

--- src/aws-sso-cli/ssocli.py
+++ src/aws-sso-cli/ssocli.py
@@ -34,46 +34,36 @@ print('NETBIOS Name: ' + args.netbios)
 print('SSO Region: ' + args.ssoregion)
 print('STS Region: ' + args.stsregion + '\n\r')
 
-if not args.password:
+def get_authentication_code():
+    if args.verbose:
+        print('\n\rStep 1: Get Authentication Code using the AD user credentials')
+        print('-------')
+        print('   - Getting Authentication Code from ' + args.directoryurl + '...\n\r')
     try:
-        args.password = getpass.getpass('Please type password for user ' + args.login + ': ')
-    except Exception as e:
+        authcode_values = sso.get_authentication_code()
+        return authcode_values
+    except AuthCodeError as e:
         print(e)
         exit(1)
+    except Exception:
+        raise
 
-if args.verbose:
-    print('\n\rStep 1: Get Authentication Code using the AD user credentials')
-    print('-------')
-    print('   - Getting Authentication Code from ' + args.directoryurl + '...\n\r')
+def get_access_token(authcode_values):
+    if args.verbose:
+        print('\n\rStep 2: Get Access Token using the Authentication Code')
+        print('-------')
+        print('   - Exchanging Authentication code for Access Token...\n\r')
 
-sso = awssso(args.netbios, args.dirname, args.directoryurl, args.ssoregion, args.login, args.password, args.verbose)
+    try:
+        token_response = sso.get_access_token(authcode_values['authcode'],authcode_values['referer'],authcode_values['cookies'])
+        return token_response
+    except AccessTokenError as e:
+        print(e)
+        exit(1)
+    except Exception:
+        raise
 
-try:
-    authcode_values = sso.get_authentication_code()
-except AuthCodeError as e:
-    print(e)
-    exit(1)
-except Exception:
-    raise
-
-if args.verbose:
-    print('\n\rStep 2: Get Access Token using the Authentication Code')
-    print('-------')
-    print('   - Exchanging Authentication code for Access Token...\n\r')
-
-try:
-    token_response = sso.get_access_token(authcode_values['authcode'],authcode_values['referer'],authcode_values['cookies'])
-except AccessTokenError as e:
-    print(e)
-    exit(1)
-except Exception:
-    raise
-
-# If an Access Token is successfully obtained then it will be used to list the applications configured in AWS SSO for this user.
-# If appinstanceid and rolename args are not passed then it will show the list of sso applications
-# The the user copy the application instance id and pass it as an argument to this module.
-
-if not args.appinstanceid and not args.rolename:
+def list_application_instances(token_response):
     if args.verbose:
         print('Step 3: Use the Access Token to get the list of applications configured in AWS SSO')
         print('-------')
@@ -90,8 +80,7 @@ if not args.appinstanceid and not args.rolename:
         print('\n\r./ssocli.py -i ins-1becf2edf4961234')
         print('\n\rRun ./ssocli.py -h for more information about how to use this module\n\r')
 
-# If only the application instance id is passed as argument then list the roles associated to the application instance (aws account).
-elif args.appinstanceid and not args.rolename:
+def list_roles(token_response):
     if args.verbose:
         print('Step 3: Use the Access Token to get the list of profiles (roles) assigned to the AWS account')
         print('-------')
@@ -108,9 +97,7 @@ elif args.appinstanceid and not args.rolename:
         print('Run ./ssocli.py -h for more information about how to use this module\n\r')
     exit(0)
 
-# If both the application instance id and the role name are passed as arguments
-# then get the SAML assertion and call STS AssumeRoleWithSAML
-elif args.appinstanceid and args.rolename:
+def get_saml_assertion(token_response):
     if args.verbose:
         print('\n\rStep 3: Get SAML assertion for the application by using the Access Token')
         print('--------')
@@ -132,3 +119,28 @@ elif args.appinstanceid and args.rolename:
     who_am_i = json.dumps(sts.get_caller_identity()['Arn'])
     print('\n\rAssumedRoleIdentity: ' + who_am_i + '\n\r')
         
+if __name__ == '__main__':
+    if not args.password:
+        try:
+            args.password = getpass.getpass('Please type password for user ' + args.login + ': ')
+        except Exception as e:
+            print(e)
+            exit(1)
+    sso = awssso(args.netbios, args.dirname, args.directoryurl, args.ssoregion, args.login, args.password, args.verbose)
+    authcode_res = get_authentication_code()
+    token_res = get_access_token(authcode_res)
+    # If an Access Token is successfully obtained then it will be used to
+    # list the applications configured in AWS SSO for this user.
+    # If appinstanceid and rolename args are not passed then it will show the list of sso applications
+    # The the user copy the application instance id and pass it as an argument to this module.
+    if not args.appinstanceid and not args.rolename:
+        list_application_instances(token_res)
+    # If only the application instance id is passed as argument then
+    # list the roles associated to the application instance (aws account).
+    elif args.appinstanceid and not args.rolename:
+        list_roles(token_res)
+    # If both the application instance id and the role name are passed as arguments
+    # then get the SAML assertion and call STS AssumeRoleWithSAML
+    elif args.appinstanceid and args.rolename:
+        get_saml_assertion(token_res)
+
